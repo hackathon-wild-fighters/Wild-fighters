b895efd5f83b84d0ce0eb056554deffb8a7b2797
Bogdan Ceu <ceubogdan@yahoo.com>
2018-04-05 17:21:24 +0200

    test recupere photo

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-document.getElementById('app').innerHTML = "hello world"
+fetch('https://akabab.github.io/superhero-api/api/id/1.json')
+.then(res => res.json())
+.then(resJson =>{
+  console.log(resJson.images.sm)
+  document.getElementById('hero1').innerHTML = `<img "${}"`
+})
