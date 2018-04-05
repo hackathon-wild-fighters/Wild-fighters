5acaf32f55f22c67a02d235536b2f33c01d8e280
Bogdan Ceu <ceubogdan@yahoo.com>
2018-04-05 17:51:17 +0200

    test css

--- js/index.js
+++ js/index.js
@@ -2,5 +2,11 @@ fetch('https://akabab.github.io/superhero-api/api/id/1.json')
 .then(res => res.json())
 .then(resJson =>{
   console.log(resJson.images.sm)
-  document.getElementById('hero1').innerHTML = `<img "${}"`
+  document.getElementById('hero1').innerHTML = `<img src="${resJson.images.sm}">`
+})
+fetch('https://akabab.github.io/superhero-api/api/id/2.json')
+.then(res => res.json())
+.then(resJson =>{
+  console.log(resJson.images.sm)
+  document.getElementById('hero2').innerHTML = `<img src="${resJson.images.sm}">`
 })
