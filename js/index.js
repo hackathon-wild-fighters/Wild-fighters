479282484b3a00c7aec7a34f1bcf858d297db26c
Bogdan Ceu <ceubogdan@yahoo.com>
2018-04-05 19:03:59 +0200

    heros random

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
-fetch('https://akabab.github.io/superhero-api/api/id/1.json')
+const random = () =>{
+  return Math.floor(Math.random() * (500 - 1) + 1)
+}
+
+
+fetch(`https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/${random()}.json`)
 .then(res => res.json())
 .then(resJson =>{
   console.log(resJson.images.sm)
-  document.getElementById('hero1').innerHTML = `<img src="${resJson.images.sm}">`
+  document.getElementById('hero1').innerHTML = `<img src="${resJson.images.sm}" class="img-fluid" >`
 })
-fetch('https://akabab.github.io/superhero-api/api/id/2.json')
+fetch(`https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/${random()}.json`)
 .then(res => res.json())
 .then(resJson =>{
   console.log(resJson.images.sm)
-  document.getElementById('hero2').innerHTML = `<img src="${resJson.images.sm}">`
+  document.getElementById('hero2').innerHTML = `<img src="${resJson.images.sm}" class="img-fluid">`
 })
