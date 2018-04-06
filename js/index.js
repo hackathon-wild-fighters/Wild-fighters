6dfd3b697db40829075a3ca14a36a4443265e19d
Bogdan Ceu <ceubogdan@yahoo.com>
2018-04-06 08:26:29 +0200

    arena style

--- js/index.js
+++ js/index.js
@@ -50,7 +50,7 @@ let fight = 1
       "speed": 35,
       "durability": 65,
       "power": 100,
-      "combat": 85*/ 
+      "combat": 85*/
 
 fetch(`https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/${random()}.json`)
   .then(res => res.json())
@@ -75,7 +75,7 @@ const audio2 = new Audio('songs/coup2.mp3');
 //  barre de vie
 const lifeBar = (lifeHero, idName, lifeMax) => {
   document.getElementById(idName).innerHTML = `<div class="progress">
-<div class="progress-bar bg-success" role="progressbar" aria-valuemin="0" aria-valuemax="100" 
+<div class="progress-bar bg-success" role="progressbar" aria-valuemin="0" aria-valuemax="100"
 style="width: ${lifeMax}%">${lifeHero}%
 <span class="sr-only"></span>
 </div>
@@ -94,7 +94,7 @@ document.addEventListener('keydown', (event) => {
     audio.play();
     if(life1>0){
     lifeBar(life1, 'bareDeVie1', life1)
-  
+
     document.getElementById('superheros').innerHTML = `
     <div id="hero1" class="col-4 offset-1 dmgEffect"><img src="${image1}" class="img-fluid"></div>
     <div id="hero2" class="col-4 offset-2"><img src="${image2}" class="img-fluid"></div>
@@ -103,9 +103,101 @@ document.addEventListener('keydown', (event) => {
       lifeBar(0, 'bareDeVie1', 0)
       document.getElementById('superheros').innerHTML = `
     <div id="hero1" class="col-4 offset-1  gameover">GAME OVER</div>
-    <div id="hero2" class="col-4 offset-2"><img src="${image2}" class="img-fluid"></div>
+    <div id="hero2" class="col-4 offset-2 gagne"><img src="${image2}" class="img-fluid"></div>
     `}
-    
+
+  }
+})
+
+document.addEventListener('keydown', (event) => {
+  let keyName = event.key
+  console.log(event.key)
+  if (keyName === 'ArrowRight') {
+    life1 -= Math.floor(power1 / 5)
+    console.log(life1)
+    audio.play();
+    if(life1>0){
+    lifeBar(life1, 'bareDeVie1', life1)
+
+    document.getElementById('superheros').innerHTML = `
+    <div id="hero1" class="col-4 offset-1 dmg2"><img src="${image1}" class="img-fluid"></div>
+    <div id="hero2" class="col-4 offset-2"><img src="${image2}" class="img-fluid"></div>
+    `
+    }else{
+      lifeBar(0, 'bareDeVie1', 0)
+      document.getElementById('superheros').innerHTML = `
+    <div id="hero1" class="col-4 offset-1  gameover">GAME OVER</div>
+    <div id="hero2" class="col-4 offset-2 gagne"><img src="${image2}" class="img-fluid"></div>
+    `}
+
+  }
+})
+
+document.addEventListener('keydown', (event) => {
+  let keyName = event.key
+  console.log(event.key)
+  if (keyName === 'ArrowUp') {
+    life1 -= Math.floor(power1 / 5)
+    console.log(life1)
+    audio.play();
+    if(life1>0){
+    lifeBar(life1, 'bareDeVie1', life1)
+
+    document.getElementById('superheros').innerHTML = `
+    <div id="hero1" class="col-4 offset-1 dmg3"><img src="${image1}" class="img-fluid"></div>
+    <div id="hero2" class="col-4 offset-2"><img src="${image2}" class="img-fluid"></div>
+    `
+    }else{
+      lifeBar(0, 'bareDeVie1', 0)
+      document.getElementById('superheros').innerHTML = `
+    <div id="hero1" class="col-4 offset-1  gameover">GAME OVER</div>
+    <div id="hero2" class="col-4 offset-2 gagne"><img src="${image2}" class="img-fluid"></div>
+    `}
+
+  }
+})
+
+document.addEventListener('keydown', (event) => {
+  let keyName = event.key
+  console.log(event.key)
+  if (keyName === 's') {
+    life2 -= Math.floor(power2 / 5)
+    console.log(life2)
+    audio2.play();
+    if(life2>0){
+    lifeBar(life2, 'bareDeVie2', life2)
+    document.getElementById('superheros').innerHTML = `
+    <div id="hero1" class="col-4 offset-1"><img src="${image1}" class="img-fluid"></div>
+    <div id="hero2" class="col-4 offset-2 dmgEffect"><img src="${image2}" class="img-fluid"></div>
+    `
+    }else{
+      lifeBar(0, 'bareDeVie2', 0)
+      document.getElementById('superheros').innerHTML = `
+    <div id="hero1" class="col-4 offset-1 gagne "><img src="${image1}" class="img-fluid"></div>
+    <div id="hero2" class="col-4 offset-2 gameover">GAME OVER</div>`
+    }
+  }
+})
+
+document.addEventListener('keydown', (event) => {
+  let keyName = event.key
+  console.log(event.key)
+  if (keyName === 'f') {
+    life2 -= Math.floor(power2 / 5)
+    console.log(life2)
+    audio2.play();
+    if(life2>0){
+    lifeBar(life2, 'bareDeVie2', life2)
+    document.getElementById('superheros').innerHTML = `
+    <div id="hero1" class="col-4 offset-1"><img src="${image1}" class="img-fluid"></div>
+    <div id="hero2" class="col-4 offset-2 dmg2"><img src="${image2}" class="img-fluid"></div>
+    `
+    }else{
+      lifeBar(0, 'bareDeVie2', 0)
+      document.getElementById('superheros').innerHTML = `
+    <div id="hero1" class="col-4 offset-1 gagne "><img src="${image1}" class="img-fluid"></div>
+    <div id="hero2" class="col-4 offset-2 gameover">GAME OVER</div>`
+    }
   }
 })
 
@@ -120,12 +212,12 @@ document.addEventListener('keydown', (event) => {
     lifeBar(life2, 'bareDeVie2', life2)
     document.getElementById('superheros').innerHTML = `
     <div id="hero1" class="col-4 offset-1"><img src="${image1}" class="img-fluid"></div>
-    <div id="hero2" class="col-4 offset-2 dmgEffect"><img src="${image2}" class="img-fluid"></div>
+    <div id="hero2" class="col-4 offset-2 dmg3"><img src="${image2}" class="img-fluid"></div>
     `
     }else{
       lifeBar(0, 'bareDeVie2', 0)
       document.getElementById('superheros').innerHTML = `
-    <div id="hero1" class="col-4 offset-1  "><img src="${image1}" class="img-fluid"></div>
+    <div id="hero1" class="col-4 offset-1 gagne "><img src="${image1}" class="img-fluid"></div>
     <div id="hero2" class="col-4 offset-2 gameover">GAME OVER</div>`
     }
   }
