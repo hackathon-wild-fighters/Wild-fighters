f971dbe9c520b12aae1aa1250a0aa56e22a7fd07
SophieMdl <sophie.michaudel@gmail.com>
2018-04-06 10:05:58 +0200

    corrections

--- js/fight.js
+++ js/fight.js
@@ -1,11 +1,12 @@
-const random = () => {
-  return Math.floor(Math.random() * (500 - 1) + 1)
-}
+export const fight = () => {
+  const random = () => {
+    return Math.floor(Math.random() * (500 - 1) + 1)
+  }
 
-const randomBackground =() =>{
-  return Math.floor(Math.random()*(9-1)+1)
-}
-document.getElementById('main').innerHTML =`<div id="arena${randomBackground()}">
+  const randomBackground = () => {
+    return Math.floor(Math.random() * (9 - 1) + 1)
+  }
+  document.getElementById('main').innerHTML = `<div id="arena${randomBackground()}">
 <div class="container-fluid">
 <div class="baredevie row">
   <div id= "bareDeVie1" class=" col-5">100%</div>
@@ -35,191 +36,192 @@ document.getElementById('main').innerHTML =`<div id="arena${randomBackground()}"
 </div>
 </div>
 </div>`
-let image1 = ''
-let image2 = ''
+  let image1 = ''
+  let image2 = ''
 
-let power1 = ''
-let power2 = ''
-let pouv = 1
-let int = 1
-let force = 1
-let fight = 1
+  let power1 = ''
+  let power2 = ''
+  let pouv = 1
+  let int = 1
+  let force = 1
+  let fight = 1
 
-/* "intelligence": 88,
-      "strength": 28,
-      "speed": 35,
-      "durability": 65,
-      "power": 100,
-      "combat": 85*/ 
+  /* "intelligence": 88,
+        "strength": 28,
+        "speed": 35,
+        "durability": 65,
+        "power": 100,
+        "combat": 85*/
 
-fetch(`https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/${random()}.json`)
-  .then(res => res.json())
-  .then(resJson => {
-    power1 = ((resJson.powerstats.power + pouv)+ (resJson.powerstats.intelligence + int) + (resJson.powerstats.strength + force) + (resJson.powerstats.combat + fight)) / 4
-    image1 = resJson.images.sm
-    document.getElementById('hero1').innerHTML = `<img src="${resJson.images.sm}" class="img-fluid" >`
-  })
-fetch(`https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/${random()}.json`)
-  .then(res => res.json())
-  .then(resJson => {
-    power2 = (resJson.powerstats.power + resJson.powerstats.intelligence + resJson.powerstats.strength + resJson.powerstats.combat) / 4
-    image2 = resJson.images.sm
-    document.getElementById('hero2').innerHTML = `<img src="${resJson.images.sm}" class="img-fluid">`
-  })
+  // fetch(`https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/${random()}.json`)
+  //   .then(res => res.json())
+  //   .then(resJson => {
+  power1 = ((resJson.powerstats.power + pouv) + (resJson.powerstats.intelligence + int) + (resJson.powerstats.strength + force) + (resJson.powerstats.combat + fight)) / 4
+  image1 = resJson.images.sm
+  document.getElementById('hero1').innerHTML = `<img src="${resJson.images.sm}" class="img-fluid" >`
+  // })
+  // fetch(`https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/${random()}.json`)
+  //   .then(res => res.json())
+  //   .then(resJson => {
+  power2 = (resJson.powerstats.power + resJson.powerstats.intelligence + resJson.powerstats.strength + resJson.powerstats.combat) / 4
+  image2 = resJson.images.sm
+  document.getElementById('hero2').innerHTML = `<img src="${resJson.images.sm}" class="img-fluid">`
+  // })
 
-let life1 = 100
-let life2 = 100
-const audio = new Audio('songs/coup.mp3');
-const audio2 = new Audio('songs/coup2.mp3');
-const MusicMenu = new Audio('songs/menuMusic.mp3');
-MusicMenu.play()
-//  barre de vie
-const lifeBar = (lifeHero, idName, lifeMax) => {
-  document.getElementById(idName).innerHTML = `<div class="progress">
+  let life1 = 100
+  let life2 = 100
+  const audio = new Audio('songs/coup.mp3');
+  const audio2 = new Audio('songs/coup2.mp3');
+  const MusicMenu = new Audio('songs/menuMusic.mp3');
+  MusicMenu.play()
+  //  barre de vie
+  const lifeBar = (lifeHero, idName, lifeMax) => {
+    document.getElementById(idName).innerHTML = `<div class="progress">
 <div class="progress-bar bg-success" role="progressbar" aria-valuemin="0" aria-valuemax="100" 
 style="width: ${lifeMax}%">${lifeHero}%
 <span class="sr-only"></span>
 </div>
 </div>`
-}
+  }
 
-lifeBar(life1, 'bareDeVie1', life1)
-lifeBar(life2, 'bareDeVie2', life2)
-//appuie sur touche -> reduit barre de vie + effet image
-document.addEventListener('keydown', (event) => {
-  let keyName = event.key
-  console.log(event.key)
-  if (keyName === 'ArrowLeft') {
-    life1 -= Math.floor(power1 / 5)
-    console.log(life1)
-    audio.play();
-    if(life1>0){
-    lifeBar(life1, 'bareDeVie1', life1)
-  
-    document.getElementById('superheros').innerHTML = `
+  lifeBar(life1, 'bareDeVie1', life1)
+  lifeBar(life2, 'bareDeVie2', life2)
+  //appuie sur touche -> reduit barre de vie + effet image
+  document.addEventListener('keydown', (event) => {
+    let keyName = event.key
+    console.log(event.key)
+    if (keyName === 'ArrowLeft') {
+      life1 -= Math.floor(power1 / 5)
+      console.log(life1)
+      audio.play();
+      if (life1 > 0) {
+        lifeBar(life1, 'bareDeVie1', life1)
+
+        document.getElementById('superheros').innerHTML = `
     <div id="hero1" class="col-4 offset-1 dmgEffect"><img src="${image1}" class="img-fluid"></div>
     <div id="hero2" class="col-4 offset-2"><img src="${image2}" class="img-fluid"></div>
     `
-    }else{
-      lifeBar(0, 'bareDeVie1', 0)
-      document.getElementById('superheros').innerHTML = `
+      } else {
+        lifeBar(0, 'bareDeVie1', 0)
+        document.getElementById('superheros').innerHTML = `
     <div id="hero1" class="col-4 offset-1  gameover">GAME OVER</div>
     <div id="hero2" class="col-4 offset-2 gagne"><img src="${image2}" class="img-fluid"></div>
     `}
-    
-  }
-})
 
-document.addEventListener('keydown', (event) => {
-  let keyName = event.key
-  console.log(event.key)
-  if (keyName === 'ArrowRight') {
-    life1 -= Math.floor(power1 / 5)
-    console.log(life1)
-    audio.play();
-    if(life1>0){
-    lifeBar(life1, 'bareDeVie1', life1)
-  
-    document.getElementById('superheros').innerHTML = `
+    }
+  })
+
+  document.addEventListener('keydown', (event) => {
+    let keyName = event.key
+    console.log(event.key)
+    if (keyName === 'ArrowRight') {
+      life1 -= Math.floor(power1 / 5)
+      console.log(life1)
+      audio.play();
+      if (life1 > 0) {
+        lifeBar(life1, 'bareDeVie1', life1)
+
+        document.getElementById('superheros').innerHTML = `
     <div id="hero1" class="col-4 offset-1 dmg2"><img src="${image1}" class="img-fluid"></div>
     <div id="hero2" class="col-4 offset-2"><img src="${image2}" class="img-fluid"></div>
     `
-    }else{
-      lifeBar(0, 'bareDeVie1', 0)
-      document.getElementById('superheros').innerHTML = `
+      } else {
+        lifeBar(0, 'bareDeVie1', 0)
+        document.getElementById('superheros').innerHTML = `
     <div id="hero1" class="col-4 offset-1  gameover">GAME OVER</div>
     <div id="hero2" class="col-4 offset-2 gagne"><img src="${image2}" class="img-fluid"></div>
     `}
-    
-  }
-})
 
-document.addEventListener('keydown', (event) => {
-  let keyName = event.key
-  console.log(event.key)
-  if (keyName === 'ArrowUp') {
-    life1 -= Math.floor(power1 / 5)
-    console.log(life1)
-    audio.play();
-    if(life1>0){
-    lifeBar(life1, 'bareDeVie1', life1)
-  
-    document.getElementById('superheros').innerHTML = `
+    }
+  })
+
+  document.addEventListener('keydown', (event) => {
+    let keyName = event.key
+    console.log(event.key)
+    if (keyName === 'ArrowUp') {
+      life1 -= Math.floor(power1 / 5)
+      console.log(life1)
+      audio.play();
+      if (life1 > 0) {
+        lifeBar(life1, 'bareDeVie1', life1)
+
+        document.getElementById('superheros').innerHTML = `
     <div id="hero1" class="col-4 offset-1 dmg3"><img src="${image1}" class="img-fluid"></div>
     <div id="hero2" class="col-4 offset-2"><img src="${image2}" class="img-fluid"></div>
     `
-    }else{
-      lifeBar(0, 'bareDeVie1', 0)
-      document.getElementById('superheros').innerHTML = `
+      } else {
+        lifeBar(0, 'bareDeVie1', 0)
+        document.getElementById('superheros').innerHTML = `
     <div id="hero1" class="col-4 offset-1  gameover">GAME OVER</div>
     <div id="hero2" class="col-4 offset-2 gagne"><img src="${image2}" class="img-fluid"></div>
     `}
-    
-  }
-})
 
-document.addEventListener('keydown', (event) => {
-  let keyName = event.key
-  console.log(event.key)
-  if (keyName === 's') {
-    life2 -= Math.floor(power2 / 5)
-    console.log(life2)
-    audio2.play();
-    if(life2>0){
-    lifeBar(life2, 'bareDeVie2', life2)
-    document.getElementById('superheros').innerHTML = `
+    }
+  })
+
+  document.addEventListener('keydown', (event) => {
+    let keyName = event.key
+    console.log(event.key)
+    if (keyName === 's') {
+      life2 -= Math.floor(power2 / 5)
+      console.log(life2)
+      audio2.play();
+      if (life2 > 0) {
+        lifeBar(life2, 'bareDeVie2', life2)
+        document.getElementById('superheros').innerHTML = `
     <div id="hero1" class="col-4 offset-1"><img src="${image1}" class="img-fluid"></div>
     <div id="hero2" class="col-4 offset-2 dmgEffect"><img src="${image2}" class="img-fluid"></div>
     `
-    }else{
-      lifeBar(0, 'bareDeVie2', 0)
-      document.getElementById('superheros').innerHTML = `
+      } else {
+        lifeBar(0, 'bareDeVie2', 0)
+        document.getElementById('superheros').innerHTML = `
     <div id="hero1" class="col-4 offset-1  gagne"><img src="${image1}" class="img-fluid"></div>
     <div id="hero2" class="col-4 offset-2 gameover">GAME OVER</div>`
+      }
     }
-  }
-})
+  })
 
-document.addEventListener('keydown', (event) => {
-  let keyName = event.key
-  console.log(event.key)
-  if (keyName === 'f') {
-    life2 -= Math.floor(power2 / 5)
-    console.log(life2)
-    audio2.play();
-    if(life2>0){
-    lifeBar(life2, 'bareDeVie2', life2)
-    document.getElementById('superheros').innerHTML = `
+  document.addEventListener('keydown', (event) => {
+    let keyName = event.key
+    console.log(event.key)
+    if (keyName === 'f') {
+      life2 -= Math.floor(power2 / 5)
+      console.log(life2)
+      audio2.play();
+      if (life2 > 0) {
+        lifeBar(life2, 'bareDeVie2', life2)
+        document.getElementById('superheros').innerHTML = `
     <div id="hero1" class="col-4 offset-1"><img src="${image1}" class="img-fluid"></div>
     <div id="hero2" class="col-4 offset-2 dmg2"><img src="${image2}" class="img-fluid"></div>
     `
-    }else{
-      lifeBar(0, 'bareDeVie2', 0)
-      document.getElementById('superheros').innerHTML = `
+      } else {
+        lifeBar(0, 'bareDeVie2', 0)
+        document.getElementById('superheros').innerHTML = `
     <div id="hero1" class="col-4 offset-1 gagne "><img src="${image1}" class="img-fluid"></div>
     <div id="hero2" class="col-4 offset-2 gameover">GAME OVER</div>`
+      }
     }
-  }
-})
+  })
 
-document.addEventListener('keydown', (event) => {
-  let keyName = event.key
-  console.log(event.key)
-  if (keyName === 'e') {
-    life2 -= Math.floor(power2 / 5)
-    console.log(life2)
-    audio2.play();
-    if(life2>0){
-    lifeBar(life2, 'bareDeVie2', life2)
-    document.getElementById('superheros').innerHTML = `
+  document.addEventListener('keydown', (event) => {
+    let keyName = event.key
+    console.log(event.key)
+    if (keyName === 'e') {
+      life2 -= Math.floor(power2 / 5)
+      console.log(life2)
+      audio2.play();
+      if (life2 > 0) {
+        lifeBar(life2, 'bareDeVie2', life2)
+        document.getElementById('superheros').innerHTML = `
     <div id="hero1" class="col-4 offset-1"><img src="${image1}" class="img-fluid"></div>
     <div id="hero2" class="col-4 offset-2 dmg3"><img src="${image2}" class="img-fluid"></div>
     `
-    }else{
-      lifeBar(0, 'bareDeVie2', 0)
-      document.getElementById('superheros').innerHTML = `
+      } else {
+        lifeBar(0, 'bareDeVie2', 0)
+        document.getElementById('superheros').innerHTML = `
     <div id="hero1" class="col-4 offset-1 gagne "><img src="${image1}" class="img-fluid"></div>
     <div id="hero2" class="col-4 offset-2 gameover">GAME OVER</div>`
+      }
     }
-  }
-})
+  })
+}
--- js/navigation-ui.js
+++ js/navigation-ui.js
@@ -2,37 +2,6 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
     const leftTab = document.querySelectorAll('#perso_grid .img_perso')
     const rightTab = document.querySelectorAll('#perso_grid2 .img_perso')
 
-    const spells = [
-        { "spell": "tourbillon de lames", "img": "spellList/icons_spells/tourbillonDeLames.jpg" },
-        { "spell": "toucher corrosif", "img": "spellList/icons_spells/toucherCorrosif.jpg" },
-        { "spell": "toucher contaminé", "img": "spellList/icons_spells/toucherContamine.jpg" },
-        { "spell": "Terres brûlées", "img": "spellList/icons_spells/terresBrulees.jpg" },
-        { "spell": "souffle de glace", "img": "spellList/icons_spells/souffleDeGlace.jpg" },
-        { "spell": "Rayon plasma", "img": "spellList/icons_spells/rayonPlasma.jpg" },
-        { "spell": "putréfaction", "img": "spellList/icons_spells/putrefaction.jpg" },
-        { "spell": "points de suture", "img": "spellList/icons_spells/pointsDeSuture.jpg" },
-        { "spell": "pluie de feu", "img": "spellList/icons_spells/pluieDeFeu.jpg" },
-        { "spell": "Nuage de poison", "img": "spellList/icons_spells/nuageDePoison.jpg" },
-        { "spell": "mur de flamme", "img": "spellList/icons_spells/murDeFlamme.jpg" },
-        { "spell": "hachoir", "img": "spellList/icons_spells/hachoir.jpg" },
-        { "spell": "gel", "img": "spellList/icons_spells/gel.jpg" },
-        { "spell": "Fulgurance des arcanes", "img": "spellList/icons_spells/fulguranceDesArcanes.jpg" },
-        { "spell": "Exécution", "img": "spellList/icons_spells/execution.jpg" },
-        { "spell": "étreinte fantomatique", "img": "spellList/icons_spells/etreinteFantomatique.jpg" },
-        { "spell": "entailles sanglantes", "img": "spellList/icons_spells/entaillesSanglantes.jpg" },
-        { "spell": "énergie pure", "img": "spellList/icons_spells/energiePure.jpg" },
-        { "spell": "energie noire", "img": "spellList/icons_spells/energieNoire.jpg" },
-        { "spell": "crochet sanglant", "img": "spellList/icons_spells/crochetSanglant.jpg" },
-        { "spell": "chaine d'éclairs", "img": "spellList/icons_spells/chaineEclairs.jpg" },
-        { "spell": "caresse élémentaire", "img": "spellList/icons_spells/caresseElementaire.jpg" },
-        { "spell": "brise-crâne", "img": "spellList/icons_spells/briseCrane.jpg" },
-        { "spell": "brasier elementaire", "img": "spellList/icons_spells/brasierElementaire.jpg" },
-        { "spell": "attaque toxique", "img": "spellList/icons_spells/attaqueToxique.jpg" },
-        { "spell": "attaque du phénix", "img": "spellList/icons_spells/attaqueDuPhenix.jpg" },
-        { "spell": "attaque cérébral", "img": "spellList/icons_spells/attaqueCerebral.jpg" },
-        { "spell": "pluie de plasma", "img": "spellList/icons_spells/pluieDePlasma.jpg" },
-        { "spell": "mauvais contact", "img": "spellList/icons_spells/mauvaisContact.jpg" }
-    ]
     let currentPositionPlayer1 = 0
     let currentPositionPlayer2 = 0
 
@@ -41,8 +10,8 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
 
 
 
-    const player1selection = []
-    const player2selection = []
+    let player1selection = undefined
+    let player2selection = undefined
 
 
     // const screen1
@@ -51,7 +20,7 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
 
     let player1id = document.querySelector('#perso_grid .active').id
     let player2id = document.querySelector('#perso_grid2 .active').id
-
+    
     //inject stats on loadingc
     poolHeroJ1.map(elem => {
         if (elem.name === player1id)
@@ -91,7 +60,7 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
     window.addEventListener('keydown', (e) => {
         moveCursor(e)
     })
-
+    
     const moveCursor = (e) => {
         const activeTabPlayer1 = screen1.querySelector('#perso_grid .active')
         const activeTabPlayer2 = screen2.querySelector('#perso_grid2 .active')
@@ -143,7 +112,9 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
 
         //screen1 UP
         if (e.which === 90) {
-            if (currentPositionPlayer1 >= 3) {
+            console.log(player1selection)
+            if (currentPositionPlayer1 >= 3 && !player1selection) {
+                console.log('test')
                 activeTabPlayer1.classList.remove('active')
                 currentPositionPlayer1 -= 3
                 leftTab[currentPositionPlayer1].classList.add('active')
@@ -153,8 +124,8 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
         }
 
         //screen1 DOWN
-        if (e.which === 83) {
-            if (currentPositionPlayer1 < 9) {
+        else if (e.which === 83) {
+            if (currentPositionPlayer1 < 9 && !player1selection) {
                 activeTabPlayer1.classList.remove('active')
                 currentPositionPlayer1 += 3
                 leftTab[currentPositionPlayer1].classList.add('active')
@@ -164,8 +135,8 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
         }
 
         //screen1 RIGHT
-        if (e.which === 68) {
-            if (currentPositionPlayer1 < 11) {
+        else if (e.which === 68) {
+            if (currentPositionPlayer1 < 11 && !player1selection) {
                 activeTabPlayer1.classList.remove('active');
                 next.classList.add('active')
                 currentPositionPlayer1 += 1
@@ -175,8 +146,8 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
         }
 
         //screen1 LEFT
-        if (e.which === 81) {
-            if (currentPositionPlayer1 > 0) {
+        else if (e.which === 81) {
+            if (currentPositionPlayer1 > 0 && !player1selection) {
                 activeTabPlayer1.classList.remove('active');
                 prev.classList.add('active')
                 currentPositionPlayer1 -= 1
@@ -185,20 +156,9 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
             }
         }
 
-        // verrouiller son héros J1
-        if (e.which === 69) {
-            player1id = document.querySelector('#perso_grid .active').id
-            let i = 0
-            while (poolHeroJ1[i]) {
-                if (player1id === poolHeroJ1[i].name)
-                    player1selection[0] = poolHeroJ1[i]
-                i++
-            }
-        }
-
         //screen2 UP
-        if (e.which === 38) {
-            if (currentPositionPlayer2 >= 3) {
+        else if (e.which === 38) {
+            if (currentPositionPlayer2 >= 3 && !player2selection) {
                 activeTabPlayer2.classList.remove('active')
                 currentPositionPlayer2 -= 3
                 rightTab[currentPositionPlayer2].classList.add('active')
@@ -207,8 +167,8 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
             }
         }
         //screen2 DOWN
-        if (e.which === 40) {
-            if (currentPositionPlayer2 < 9) {
+        else if (e.which === 40) {
+            if (currentPositionPlayer2 < 9 && !player2selection) {
                 activeTabPlayer2.classList.remove('active')
                 currentPositionPlayer2 += 3
                 rightTab[currentPositionPlayer2].classList.add('active')
@@ -217,8 +177,8 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
             }
         }
         //screen2 RIGHT
-        if (e.which === 39) {
-            if (currentPositionPlayer2 < 11) {
+        else if (e.which === 39) {
+            if (currentPositionPlayer2 < 11 && !player2selection) {
                 activeTabPlayer2.classList.remove('active');
                 next2.classList.add('active')
                 currentPositionPlayer2 += 1
@@ -227,25 +187,40 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
             }
         }
         //screen2 left
-        if (e.which === 37) {
-            if (currentPositionPlayer2 > 0) {
-                activeTabPlayer2.classList.remove('active');
+        else if(e.which === 37) {
+            if (currentPositionPlayer2 > 0 && !player2selection) {
+                activeTabPlayer2.classList.remove('active')
                 prev2.classList.add('active')
                 currentPositionPlayer2 -= 1
                 player2id = document.querySelector('#perso_grid2 .active').id
                 injectJ2info()
             }
         }
-
+        // verrouiller son héros J1
+        else if (e.which === 69) {
+            activeTabPlayer1.classList.add('selection')
+            player1id = activeTabPlayer1.id
+            player1selection = activeTabPlayer1.id
+        }
         // verrouiller son héros J2
-        if (e.which === 16) {
-            player2id = document.querySelector('#perso_grid2 .active').id
-            let i = 0
-            while (poolHeroJ2[i]) {
-                if (player2id === poolHeroJ2[i].name)
-                    player2selection[0] = poolHeroJ2[i]
-                i++
-            }
+        else if (e.which === 16) {
+            activeTabPlayer2.classList.add('selection')
+            player2id = activeTabPlayer2.id
+            player2selection = activeTabPlayer2.id
+        }
+        // Déverrouiller son héros J1
+        else if (e.which === 9 && player1selection) {
+            activeTabPlayer1.classList.remove('selection')
+            player1selection = undefined
+        }
+        // Déverrouiller son héros J2
+        else if (e.which === 17 && player2selection) {
+            activeTabPlayer2.classList.remove('selection')
+            player2selection = undefined
+        }
+        // Déverrouiller son héros J1
+        else if (e.which === 32 && player1selection && player2selection) {
+            console.log('start')
         }
 
     }
