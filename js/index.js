02b8483bceb97274152bea3119a40da922abe253
Mehdi Chtira <mehdi.chtira@gmail.com>
2018-04-06 05:13:14 +0200

    je fatigue

--- js/index.js
+++ js/index.js
@@ -1,31 +1,102 @@
 import { navigation } from './navigation-ui.js'
+const spells = [
+{"spell" : "tourbillon de lames", "img" : "spellList/icons_spells/tourbillonDeLames.jpg"},
+{"spell" : "toucher corrosif", "img" : "spellList/icons_spells/toucherCorrosif.jpg"},
+{"spell" : "toucher contaminé", "img" : "spellList/icons_spells/toucherContamine.jpg"},
+{"spell" : "Terres brûlées", "img" : "spellList/icons_spells/terresBrulees.jpg"},
+{"spell" : "souffle de glace", "img" : "spellList/icons_spells/souffleDeGlace.jpg"},
+{"spell" : "Rayon plasma", "img" : "spellList/icons_spells/rayonPlasma.jpg"},
+{"spell" : "putréfaction", "img" : "spellList/icons_spells/putrefaction.jpg"},
+{"spell" : "points de suture", "img" : "spellList/icons_spells/pointsDeSuture.jpg"},
+{"spell" : "pluie de feu", "img" : "spellList/icons_spells/pluieDeFeu.jpg"},
+{"spell" : "Nuage de poison", "img" : "spellList/icons_spells/nuageDePoison.jpg"},
+{"spell" : "mur de flamme", "img" : "spellList/icons_spells/murDeFlamme.jpg"},
+{"spell" : "hachoir", "img" : "spellList/icons_spells/hachoir.jpg"},
+{"spell" : "gel", "img" : "spellList/icons_spells/gel.jpg"},
+{"spell" : "Fulgurance des arcanes", "img" : "spellList/icons_spells/fulguranceDesArcanes.jpg"},
+{"spell" : "Exécution", "img" : "spellList/icons_spells/execution.jpg"},
+{"spell" : "étreinte fantomatique", "img" : "spellList/icons_spells/etreinteFantomatique.jpg"},
+{"spell" : "entailles sanglantes", "img" : "spellList/icons_spells/entaillesSanglantes.jpg"},
+{"spell" : "énergie pure", "img" : "spellList/icons_spells/energiePure.jpg"},
+{"spell" : "energie noire", "img" : "spellList/icons_spells/energieNoire.jpg"},
+{"spell" : "crochet sanglant", "img" : "spellList/icons_spells/crochetSanglant.jpg"},
+{"spell" : "chaine d'éclairs", "img" : "spellList/icons_spells/chaineEclairs.jpg"},
+{"spell" : "caresse élémentaire", "img" : "spellList/icons_spells/caresseElementaire.jpg"},
+{"spell" : "brise-crâne", "img" : "spellList/icons_spells/briseCrane.jpg"},
+{"spell" : "brasier elementaire", "img" : "spellList/icons_spells/brasierElementaire.jpg"},
+{"spell" : "attaque toxique", "img" : "spellList/icons_spells/attaqueToxique.jpg"},
+{"spell" : "attaque du phénix", "img" : "spellList/icons_spells/attaqueDuPhenix.jpg"},
+{"spell" : "attaque cérébral", "img" : "spellList/icons_spells/attaqueCerebral.jpg"},
+{"spell" : "pluie de plasma", "img" : "spellList/icons_spells/pluieDePlasma.jpg"},
+{"spell" : "mauvais contact", "img" : "spellList/icons_spells/mauvaisContact.jpg"}
+]
 
 fetch("https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json")
 .then(res => res.json())
 .then(json => {
+
 	const poolHeroJ1 = []
+	let poolSpellJ1 = []
 	const poolHeroJ2 = []
+	let poolSpellJ2 = []
 	let i = 0
 
+	//random spell * 10
+	const fillPoolSpell = poolSpell => 
+	{
+		let j = 0
+		let lol
+		while (poolSpell.length < 10)
+		{
+			lol = spells[(Math.floor(Math.random() * 29))]
+			if (poolSpell.includes(lol))
+				lol = spells[(Math.floor(Math.random() * 29))]
+
+			else
+			{
+				poolSpell[j] = lol
+				j++
+			}
+		}
+	}
+
+	fillPoolSpell(poolSpellJ1)
+	fillPoolSpell(poolSpellJ2)
+
+	//inject spell j1
+	document.getElementById("info-power1").innerHTML = ''
+	poolSpellJ1.map(elem => document.getElementById("info-power1").innerHTML +=
+		`<div><img class="img_spell" src="${elem.img}"><p>${elem.spell}</p></div>`
+		)
+
+	//inject spell j2
+	document.getElementById("info-power2").innerHTML = ''
+	poolSpellJ2.map(elem => document.getElementById("info-power2").innerHTML +=
+		`<div><img class="img_spell" src="${elem.img}"><p>${elem.spell}</p></div>`
+		)
+
+	i = 0
+	// random champ * 12
 	while (i < 12) {
 		poolHeroJ1[i] = json[(Math.floor(Math.random() * 563))]
 		poolHeroJ2[i] = json[(Math.floor(Math.random() * 563))]
 		i++
 	}
 
-	document.getElementById('perso_grid').innerHTML = ''
+	//inject J1 poolchamp
 	poolHeroJ1.map(elem => document.getElementById('perso_grid').innerHTML +=
 		`<div class="img_perso" id="${elem.name}">
 		<img src="${elem.images.sm}">
 		</div>`
 		)
 
-	document.getElementById('perso_grid2').innerHTML = ''
+	//inject J2 poolchamp
 	poolHeroJ2.map(elem => document.getElementById('perso_grid2').innerHTML +=
 		`<div class="img_perso" id="${elem.name}">
 		<img  src="${elem.images.sm}">
 		</div>`
 		)
+	
 	navigation(poolHeroJ1, poolHeroJ2)
 })
 
--- js/navigation-ui.js
+++ js/navigation-ui.js
@@ -8,42 +8,51 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
     const screen1 = document.querySelector('#perso_grid')
     const screen2 = document.querySelector('#perso_grid2')
 
+
+
+    const player1selection = []
+    const player2selection = []
+
+
     // const screen1
     leftTab[0].classList.add('active')
     rightTab[0].classList.add('active')
+
     let player1id = document.querySelector('#perso_grid .active').id
     let player2id = document.querySelector('#perso_grid2 .active').id
 
+    //inject stats on loadingc
     poolHeroJ1.map(elem => { 
         if (elem.name === player1id)
             document.getElementById('info-perso1').innerHTML = `
         <div class="skills_perso1">
-            <div>
-                <span class="name_perso1">${elem.name}</span><br><br>
-                Combat : ${elem.powerstats.combat}<br>
-                Intelligence : ${elem.powerstats.intelligence}<br>
-                Force : ${elem.powerstats.strength}<br>
-                Vitesse : ${elem.powerstats.speed}<br>
-                Durabilité : ${elem.powerstats.durability}<br>
-                Puissance : ${elem.powerstats.power}
-            </div>
+        <div>
+        <span class="name_perso1">${elem.name}</span><br><br>
+        Combat : ${elem.powerstats.combat}<br>
+        Intelligence : ${elem.powerstats.intelligence}<br>
+        Force : ${elem.powerstats.strength}<br>
+        Vitesse : ${elem.powerstats.speed}<br>
+        Durabilité : ${elem.powerstats.durability}<br>
+        Puissance : ${elem.powerstats.power}
+        </div>
         </div>
         `
     })
-
+    
+    //inject stats on loadingc
     poolHeroJ2.map(elem => { 
         if (elem.name === player2id)
             document.getElementById('info-perso2').innerHTML = `
         <div class="skills_perso2">
-            <div>
-                <span class="name_perso2">${elem.name}</span><br><br>
-                Combat : ${elem.powerstats.combat}<br>
-                Intelligence : ${elem.powerstats.intelligence}<br>
-                Force : ${elem.powerstats.strength}<br>
-                Vitesse : ${elem.powerstats.speed}<br>
-                Durabilité : ${elem.powerstats.durability}<br>
-                Puissance : ${elem.powerstats.power}
-            </div>
+        <div>
+        <span class="name_perso2">${elem.name}</span><br><br>
+        Combat : ${elem.powerstats.combat}<br>
+        Intelligence : ${elem.powerstats.intelligence}<br>
+        Force : ${elem.powerstats.strength}<br>
+        Vitesse : ${elem.powerstats.speed}<br>
+        Durabilité : ${elem.powerstats.durability}<br>
+        Puissance : ${elem.powerstats.power}
+        </div>
         </div>
         `
     })
@@ -68,15 +77,15 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
                 if (elem.name === player1id)
                     document.getElementById('info-perso1').innerHTML = `
                 <div class="skills_perso1">
-                    <div>
-                        <span class="name_perso1">${elem.name}</span><br><br>
-                        Combat : ${elem.powerstats.combat}<br>
-                        Intelligence : ${elem.powerstats.intelligence}<br>
-                        Force : ${elem.powerstats.strength}<br>
-                        Vitesse : ${elem.powerstats.speed}<br>
-                        Durabilité : ${elem.powerstats.durability}<br>
-                        Puissance : ${elem.powerstats.power}
-                    </div>
+                <div>
+                <span class="name_perso1">${elem.name}</span><br><br>
+                Combat : ${elem.powerstats.combat}<br>
+                Intelligence : ${elem.powerstats.intelligence}<br>
+                Force : ${elem.powerstats.strength}<br>
+                Vitesse : ${elem.powerstats.speed}<br>
+                Durabilité : ${elem.powerstats.durability}<br>
+                Puissance : ${elem.powerstats.power}
+                </div>
                 </div>
                 `
             })
@@ -87,15 +96,15 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
                 if (elem.name === player2id)
                     document.getElementById('info-perso2').innerHTML = `
                 <div class="skills_perso2">
-                    <div>
-                        <span class="name_perso2">${elem.name}</span><br><br>
-                        Combat : ${elem.powerstats.combat}<br>
-                        Intelligence : ${elem.powerstats.intelligence}<br>
-                        Force : ${elem.powerstats.strength}<br>
-                        Vitesse : ${elem.powerstats.speed}<br>
-                        Durabilité : ${elem.powerstats.durability}<br>
-                        Puissance : ${elem.powerstats.power}
-                    </div>
+                <div>
+                <span class="name_perso2">${elem.name}</span><br><br>
+                Combat : ${elem.powerstats.combat}<br>
+                Intelligence : ${elem.powerstats.intelligence}<br>
+                Force : ${elem.powerstats.strength}<br>
+                Vitesse : ${elem.powerstats.speed}<br>
+                Durabilité : ${elem.powerstats.durability}<br>
+                Puissance : ${elem.powerstats.power}
+                </div>
                 </div>
                 `
             })
@@ -108,23 +117,21 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
                 currentPositionPlayer1 -= 3
                 leftTab[currentPositionPlayer1].classList.add('active')
                 player1id = document.querySelector('#perso_grid .active').id
-                console.log(player1id)
                 injectJ1info()
-
             }
         }
 
+        //screen1 DOWN
         if (e.which === 83) {
             if (currentPositionPlayer1 < 9) {
                 activeTabPlayer1.classList.remove('active')
                 currentPositionPlayer1 += 3
                 leftTab[currentPositionPlayer1].classList.add('active')
                 player1id = document.querySelector('#perso_grid .active').id
-                console.log(player1id)
                 injectJ1info()
-
             }
         }
+
         //screen1 RIGHT
         if (e.which === 68) {
             if (currentPositionPlayer1 < 11) {
@@ -132,10 +139,10 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
                 next.classList.add('active')
                 currentPositionPlayer1 += 1
                 player1id = document.querySelector('#perso_grid .active').id
-                console.log(player1id)
                 injectJ1info()
             }
         }
+
         //screen1 LEFT
         if (e.which === 81) {
             if (currentPositionPlayer1 > 0) {
@@ -143,10 +150,21 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
                 prev.classList.add('active')
                 currentPositionPlayer1 -= 1
                 player1id = document.querySelector('#perso_grid .active').id
-                console.log(player1id)
                 injectJ1info()
             }
         }
+
+        // verrouiller son héros J1
+        if (e.which === 69){
+            player1id = document.querySelector('#perso_grid .active').id
+            let i = 0
+            while (poolHeroJ1[i]){
+                if (player1id === poolHeroJ1[i].name)
+                    player1selection[0] = poolHeroJ1[i]
+                i++
+            }
+        }
+
         //screen2 UP
         if (e.which === 38) {
             if (currentPositionPlayer2 >= 3) {
@@ -154,9 +172,7 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
                 currentPositionPlayer2 -= 3
                 rightTab[currentPositionPlayer2].classList.add('active')
                 player2id = document.querySelector('#perso_grid2 .active').id
-                console.log(player2id)
                 injectJ2info()
-
             }
         }
         //screen2 DOWN
@@ -166,9 +182,7 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
                 currentPositionPlayer2 += 3
                 rightTab[currentPositionPlayer2].classList.add('active')
                 player2id = document.querySelector('#perso_grid2 .active').id
-                console.log(player2id)
                 injectJ2info()
-
             }
         }
         //screen2 RIGHT
@@ -178,9 +192,7 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
                 next2.classList.add('active')
                 currentPositionPlayer2 += 1
                 player2id = document.querySelector('#perso_grid2 .active').id
-                console.log(player2id)
                 injectJ2info()
-
             }
         }
         //screen2 left
@@ -190,10 +202,20 @@ export const navigation = (poolHeroJ1, poolHeroJ2) => {
                 prev2.classList.add('active')
                 currentPositionPlayer2 -= 1
                 player2id = document.querySelector('#perso_grid2 .active').id
-                console.log(player2id)
                 injectJ2info()
-
             }
         }
+
+        // verrouiller son héros J2
+        if (e.which === 16){
+            player2id = document.querySelector('#perso_grid2 .active').id
+            let i = 0
+            while (poolHeroJ2[i]){
+                if (player2id === poolHeroJ2[i].name)
+                    player2selection[0] = poolHeroJ2[i]
+                i++
+            }
+        }
+
     }
 }
